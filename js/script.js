2288247f1086f3c0db25dc22b37a7fc5f400dc52
xeemv <xeeyang08@gmail.com>
2022-02-17 11:19:34 -0600

    created drop down list and updated js file for the drop down list

--- js/script.js
+++ js/script.js
@@ -17,18 +17,28 @@ document.getElementById('add').addEventListener('click', () => {
     //     //  it will read as current year - month - day
     //     //  plus 1 due to zero based counting for the month
     row.insertCell(1).innerHTML = document.getElementById('new-trip-date').value;
+    // user would select their an date for their planned trip
     row.insertCell(2).innerHTML = document.getElementById('new-task-destination').value;
     //  the start date that we selected at the time of creating the task will drop into this cell
     row.insertCell(3).innerHTML = document.getElementById('new-destination').value;
     //  the start date that we selected at the time of creating the task will drop into this cell
-    row.insertCell(4).innerHTML = document.getElementById('new-comments').value;
+    row.insertCell(4).innerHTML = document.getElementById('Reseached-and-logging').value;
+    // user will select from drop down list and values will display in the table
+    row.insertCell(5).innerHTML = document.getElementById('partners').value;
+    // user will select from drop down list and values will display in the table
+    row.insertCell(6).innerHTML = document.getElementById('new-comments').value;
     // row.insertCell(5).innerHTML = document.getElementById('new-comments').value;
-    let action = row.insertCell(5);
+    let action = row.insertCell(7);
+    // this is the action created to allow a user to delete the current row
     action.appendChild(createDeleteButton(id++));
     // add a button or append a button so the user could delete the button
     // it will increment
-    document.getElementById('new-task').value = "";
+    document.getElementById('new-entry-date').value = "";
     // this will give the user a blank slate to start over and add more task
+    // this will clear the entry date
+    document.getElementById('new-trip-date').value = '';
+    // this will give the user a blank slate to start over and add more task
+    // this will clear the trip date
 })
 
 function createDeleteButton(id){
@@ -43,3 +53,4 @@ function createDeleteButton(id){
     };
     return btn;
 }
+
